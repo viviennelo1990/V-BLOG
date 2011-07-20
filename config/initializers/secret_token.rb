1ee9d0e881909667f4a67f1717405c8677cc8330
luo <luo@gz-server.(none)>
2011-07-20 17:29:58 +0800

    viviennes blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '94b3a9b53813a9fbc5b4df8caf5eaa0ff6694cb08a6bf99da93d74cadfa77c595b69eac85fbcd21a3c8fb7ca2e79b9011bc2f4bf9ec401cd18893f8dd4c1a480'
